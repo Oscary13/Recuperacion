b3645ee023a1faf2e272f2e3b268ca66e5641f52
Oscary13 <oscary_1320114020@uptecamac.edu.mx>
2021-11-30 23:16:31 -0600

    edits

---
+++ index.php
@@ -0,0 +1,338 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title>RECUPERACIÓN xD</title>
+
+    <meta
+      name="description"
+      content="Free download theme onepage, clean and modern responsive for all"
+    />
+    <meta
+      name="keywords"
+      content="responsive, html5, onepage, themes, template, clean layout, free web"
+    />
+    <meta name="author" content="Thomsoon.com" />
+
+    <link rel="shortcut icon" href="img/favicon.png" />
+
+    <link rel="stylesheet" type="text/css" href="css/reset.css" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link rel="stylesheet" type="text/css" href="css/style-responsive.css" />
+  </head>
+
+  <body>
+    <div class="container">
+      <!-- section start-page -->
+
+      <section class="start-page parallax-background" id="home">
+        <div class="opacity"></div>
+        <div class="content">
+          <div class="text">
+            <h1>
+              BIENVENIDO<br />
+              <span>Venta de peces OBB</span>
+            </h1>
+            <p>¡Asegúrate de darle una vida digna a tu mascota!</p>
+
+            <a href="#about-us"><div class="read-more">Lee más</div></a>
+          </div>
+          <div class="arrow-down"></div>
+        </div>
+      </section>
+
+      <!-- section menu mobile -->
+
+      <section class="menu-media">
+        <div class="menu-content">
+          <div class="logo">OBB</div>
+
+          <div class="icon">
+            <a href="#"><img src="img/icons/menu-media.png" /></a>
+          </div>
+        </div>
+      </section>
+
+      <ul class="menu-click">
+        <li></li>
+        <a href="#home"><li href="#home">INICIO</li></a>
+        <a href="#about-us"><li href="#about-us">NOSOTROS</li></a>
+        <a href="#portfolio"><li href="#portfolio">PECES DISPONIBLES</li></a>
+        <a href="#contact"><li href="#contact">CONTACTO</li></a>
+      </ul>
+
+      <!-- section menu -->
+
+      <section class="menu">
+        <div class="menu-content">
+          <div class="logo">VENTA DE PECES</div>
+
+          <ul id="menu">
+            <li><a href="#home">INICIO</a></li>
+            <li><a href="#about-us">NOSOTROS</a></li>
+            <li><a href="#portfolio">PECES DISPONIBLES</a></li>
+            <li><a href="#contact">CONTACTO</a></li>
+          </ul>
+        </div>
+      </section>
+
+      <!-- section about us -->
+
+      <section class="about-us" id="about-us">
+        <h1>NOSOTROS</h1>
+        <hr />
+        <p class="title">
+          Se le da la más cordial bienvenida a esta pagina donde además de
+          presentarnos, te ofrecemos información del maravilloso mundo animal,
+          así como las variedades de peces que manejamos en nuestra Empresa para
+          venta Al menudeo y al Mayoreo.
+        </p>
+
+        <div class="column-one">
+          <div class="circle-one"></div>
+
+          <h2>MISIÓN:</h2>
+          <p>
+            Ser una empresa competitiva en la comercialización y distribución de
+            peces vivos de ornato y accesorios relacionados con el acuarismo que
+            cubran satisfactoriamente las necesidades y expectativas de nuestros
+            clientes.
+          </p>
+        </div>
+
+        <div class="column-two">
+          <div class="circle-two"></div>
+
+          <h2>VISIÓN:</h2>
+          <p>
+            Ser una empresa Líder Nacional en la comercialización de peces vivos
+            de ornato y accesorios para peces y otras mascotas. Una empresa que
+            se caracterice por infundir y difundir el conocimiento de dar una
+            vida digna a cada mascota.
+          </p>
+        </div>
+
+        <div class="column-three">
+          <div class="circle-three"></div>
+
+          <h2>NUESTROS VALORES:</h2>
+          <p>
+            Respeto: nuestros clientes son atendidos con esmero demostrándoles
+            aprecio. <br />
+            <strong>Responsabilidad:</strong> se cumplen profesionalmente las
+            tareas. <br /><strong>Honestidad:</strong> Somos transparentes con
+            nuestros clientes <br />
+            <strong>Compromiso:</strong> Ser una empresa social y ecológicamente
+            responsable.
+          </p>
+        </div>
+        <br /><br />
+      </section>
+
+      <div class="clear"></div>
+
+      <!-- portoflio-->
+
+      <section class="portfolio" id="portfolio">
+        <div class="portfolio-margin">
+          <h1>PECES DISPONIBLES</h1>
+          <hr />
+
+          <!-- 1 item portoflio-->
+
+          <ul class="grid">
+            <li>
+              <a href="#">
+                <img src="https://http2.mlstatic.com/D_NQ_NP_617718-MLM31107532273_062019-O.webp" alt="Portfolio item" />
+                <div class="text">
+                  <p>Pez Ranchu Negro Importado Shangai</p>
+                  <p class="description"><strong>$ 599 pesos</strong></p>
+                </div>
+              </a>
+            </li>
+
+            <!-- 2 item portoflio-->
+
+            <li>
+              <a href="#">
+                <img src="https://http2.mlstatic.com/D_NQ_NP_664742-MLM47905417873_102021-O.webp" alt="Portfolio item" />
+                <div class="text">
+                  <p>Pez Payaso</p>
+                  <p class="description">$ 330 pesos</p>
+                </div>
+              </a>
+            </li>
+
+            <!-- 3 item portoflio-->
+
+            <li>
+              <a href="#">
+                <img src="https://http2.mlstatic.com/D_NQ_NP_189515-MLM25250136031_122016-O.webp" alt="Portfolio item" />
+                <div class="text">
+                  <p>Camaron Pimienta</p>
+                  <p class="description">$ 399 pesos</p>
+                </div>
+              </a>
+            </li>
+
+            <!-- 4 item portoflio-->
+
+            <li>
+              <a href="#">
+                <img src="https://http2.mlstatic.com/D_NQ_NP_660493-MLM44168867360_112020-O.webp" alt="Portfolio item" />
+                <div class="text">
+                  <p>Pez Payaso Blackstorm</p>
+                  <p class="description">$ 2,500 pesos</p>
+                </div>
+              </a>
+            </li>
+
+            <!-- 5 item portoflio-->
+
+            <li>
+              <a href="#">
+                <img src="https://http2.mlstatic.com/D_NQ_NP_817068-MLM31211324689_062019-O.webp" alt="Portfolio item" />
+                <div class="text">
+                  <p>Camaron Arlequin</p>
+                  <p class="description">$ 2,199 pesos</p>
+                </div>
+              </a>
+            </li>
+
+            <!-- 6 item portoflio-->
+
+            <li>
+              <a href="#">
+                <img src="https://http2.mlstatic.com/D_NQ_NP_634588-MLM31222275914_062019-O.webp" alt="Portfolio item" />
+                <div class="text">
+                  <p>Pez Marino Labrido Seis Lineas</p>
+                  <p class="description">$ 950 pesos</p>
+                </div>
+              </a>
+            </li>
+
+            <!-- 7 item portoflio-->
+
+            <li>
+              <a href="#">
+                <img src="https://http2.mlstatic.com/D_NQ_NP_745108-MLM31223978567_062019-O.webp" alt="Portfolio item" />
+                <div class="text">
+                  <p>Pez Damisela Chromis Verde</p>
+                  <p class="description">$ 399 pesos</p>
+                </div>
+              </a>
+            </li>
+
+            <!-- 8 item portoflio-->
+
+            <li>
+              <a href="#">
+                <img src="https://http2.mlstatic.com/D_NQ_NP_790258-MLM31701396864_082019-O.webp" alt="Portfolio item" />
+                <div class="text">
+                  <p>Pez Cirujano Naso</p>
+                  <p class="description">$ 1,999 pesos</p>
+                </div>
+              </a>
+            </li>
+
+            <!-- 9 item portoflio-->
+
+            <li>
+              <a href="#">
+                <img src="https://http2.mlstatic.com/D_NQ_NP_901978-MLM43389503821_092020-O.webp" alt="Portfolio item" />
+                <div class="text">
+                  <p>Pez Gobio Neon</p>
+                  <p class="description">$ 450 pesos</p>
+                </div>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <div class="clear"></div>
+
+      <!-- partners-->
+
+      <section class="partners parallax-background-partners" id="partners">
+        <div class="opacity"></div>
+
+        <div class="content">
+          <h2>Gracias a nuestros socios</h2>
+
+          <div class="logo">
+            <a href="#"><img src="img/logos/alex1.png" /></a>
+            <a href="#"><img src="img/logos/archiq.png" /></a>
+            <a href="#"><img src="img/logos/thomsoon.png" /></a>
+            <a href="#"><img src="img/logos/alex2.png" /></a>
+          </div>
+        </div>
+      </section>
+
+      <!-- Contact-->
+
+      <section class="contact" id="contact">
+        <h1>CONTACTO</h1>
+        <hr />
+
+        <div class="content">
+          <div class="form">
+            <form action="#" method="post" enctype="text/plain">
+              <input name="your-name" id="your-name" value="TU NOMBRE" />
+              <input name="your-email" id="your-email" value="TU CORREO ELECTRÓNICO" />
+
+              <textarea id="message" name="message">MENSAJE</textarea>
+
+              <a href="#">
+                <div class="button">
+                  <span>ENVIAR</span>
+                </div>
+              </a>
+            </form>
+          </div>
+
+          <div class="contact-text">
+            Para contactarnos por favor use el formulario de contacto visible<br /><br />
+
+            Al enviar archivos, utilice<br />
+            el siguiente correo electrónico<br /><br />
+
+            <strong>OSCARY BASURTO BASURTO</strong><br /><br />
+
+            Email: <strong>oscary_1320114020@uptecamac.edu.mx</strong>
+          </div>
+        </div>
+      </section>
+
+      <section class="footer">
+        <div class="margin">
+          <div class="menu-footer">
+            <a href="#home"> Inicio </a>
+            <a href="#"> Política de privacidad </a>
+            <a href="https://www.facebook.com/oscary.basurto.9/" target="_blank"> Facebook </a>
+          </div>
+
+          <div class = "copyright"> © 2021. Todos los derechos reservados OBB </div>
+        </div>
+      </section>
+    </div>
+
+    <!-- Scripts -->
+
+    <script
+      type="text/javascript"
+      src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"
+    ></script>
+    <!-- jQuery -->
+    <script src="js/jquery.parallax.js"></script>
+    <!-- jQuery Parallax -->
+    <script src="js/jquery.nicescroll.js"></script>
+    <!-- jQuery NiceScroll -->
+    <script src="js/jquery.sticky.js"></script>
+    <!-- jQuery Stick Menu -->
+    <script src="js/script.js"></script>
+    <!-- All script -->
+  </body>
+</html>
